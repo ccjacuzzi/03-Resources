c4cec95c7026faaff1ece93f858c7eea708b3932
irconde <irconde@ualr.edu>
2020-08-14 16:17:07 -0500

    Resources added

--- app/src/main/java/com/ualr/a03_resources/MainActivity.java
+++ app/src/main/java/com/ualr/a03_resources/MainActivity.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ualr.a03_resources.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // TODO 02. Set a new source image for the ImageView
+
+        // TODO 03. Set a new text for the TextView
+
+
+        // TODO 04. Set a new text color for the TextView.
+        // We cannot use the resource id. We have to pass the resolved color instead
+        // TODO 04.01. Get a reference to the current instance of the Resources class
+        // TODO 04.02. Get the actual color value
+
+        // TODO 05. How can we use the Resource class from any class.
+        // TODO 05.01. Define a new class called Utils with just one static method to get a color value based on a given color resource id
+
+        // TODO 08. Get values in the string array
+
+        // TODO 09. Use the Resource class to get a Drawable from a resource
+
+        // TODO 10. Import a new vector image using Vector Asset Studio
+        // TODO 11. Import a new vector image from the Android Material Icon library.
+        // TODO 16. Use the translatable string fo the TextView.
+
+        // TODO 17. Define a color state list to set a different background color for the button depending on the state.
     }
 }
